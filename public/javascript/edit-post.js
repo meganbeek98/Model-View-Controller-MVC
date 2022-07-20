ccf23cf363466c82bb62db64fdd77cfeaf4893de
meganbeek98 <meganbeek98@gmail.com>
2022-07-20 11:38:53 -0400

    develop branch, js

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,31 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+    
+    const title = document.querySelector('#post-title').value.trim()
+    const post_text = document.querySelector('#post-text').value.trim()
+    
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+  console.log('clicked!!' + title + post_text + id)
+  
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+    
+    document.querySelector('.edit-btn').addEventListener('submit', editFormHandler);  
